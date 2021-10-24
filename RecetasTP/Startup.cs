8e864a35c7804c0432508c2932363cd1fecbbe53
foxday <teruelpablo@gmail.com>
2021-10-24 20:10:05 -0300

    Agrega uso de sesion.

--- RecetasTP/Startup.cs
+++ RecetasTP/Startup.cs
@@ -24,6 +24,13 @@ namespace RecetasTP
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDistributedMemoryCache();
+            services.AddSession(options =>
+            {
+                options.Cookie.Name = ".TPWeb3.Session";
+                options.IdleTimeout = TimeSpan.FromSeconds(60);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +52,8 @@ namespace RecetasTP
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseSession();
 
             app.UseEndpoints(endpoints =>
             {
