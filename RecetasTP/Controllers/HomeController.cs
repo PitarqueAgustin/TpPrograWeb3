397173595501ee21fe2ca383e8f95f7ec5535c22
foxday <teruelpablo@gmail.com>
2021-10-25 00:15:18 -0300

    Ejemplo de conexto sin inyección de dependencias.

--- RecetasTP/Controllers/HomeController.cs
+++ RecetasTP/Controllers/HomeController.cs
@@ -7,16 +7,41 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using DAO.Models;
 
 namespace RecetasTP.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        
+        // Instancia el contexto del proyecto DAO
+        _20212C_TPContext ctx = new _20212C_TPContext();
+
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+        }
+
+        public IActionResult NewUser()
+        {
+            // Instancia y llena el objeto Usuario del modelo Usuario dentro de DAO/Models
+            Usuario user1 = new Usuario();
+            user1.Nombre = "Juan";
+            user1.Password = "1234";
+            user1.Email = "email@email.com";
+            user1.Perfil = 123;
+            user1.FechaRegistracion = DateTime.Now;
+
+            // Agrega al contexto el user1
+            ctx.Usuarios.Add(user1);
+
+            // Guarda en la DB el contexto
+            ctx.SaveChanges();
+
+
+            return View("/Views/Home/Index.cshtml");
         }
 
         public IActionResult Index()
