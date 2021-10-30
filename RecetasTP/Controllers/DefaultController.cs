356f4cb255221ca4f1d157bf6ad512db84e8e3dc
foxday <teruelpablo@gmail.com>
2021-10-30 19:24:00 -0300

    Prototipa la vista de home.

--- RecetasTP/Controllers/DefaultController.cs
+++ RecetasTP/Controllers/DefaultController.cs
@@ -5,6 +5,7 @@ using RecetasTP.Filters;
 using RecetasTP.Models;
 using Services.Interfaces;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 
 namespace RecetasTP.Controllers
@@ -24,10 +25,18 @@ namespace RecetasTP.Controllers
             return RedirectToAction("default");
         }
 
+
         [Route("default")]
         public IActionResult Default()
         {
             ViewBag.Layout = HttpContext.Session.GetString("layout");
+            
+            
+            List<string> imageList = new List<string>();
+            imageList.Add("34737dc8-a861-40bf-8fbd-2d207ba34376.jpg");
+            imageList.Add("71c79da2-1df7-4bfa-be55-3c72e5b8fa90.jpg");
+
+            ViewBag.ImageList = imageList;
             return View();
         }
 
