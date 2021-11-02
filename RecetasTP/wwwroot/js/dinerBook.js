a7ad9128bc0c608dbebdaddecb654d11f0ba3c4a
foxday <teruelpablo@gmail.com>
2021-11-02 15:30:06 -0300

    Implementa en forma de protitipo la ventana modal de diner/book

---
+++ RecetasTP/wwwroot/js/dinerBook.js
@@ -0,0 +1,12 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+
+$("#submit").click(function () {
+    var index = document.getElementById('index').textContent;
+    $("#modal_body").html(index);
+});
+
+
